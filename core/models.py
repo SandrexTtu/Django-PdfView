1867db937404fec24e454ff780a07d88043aec37
Aleksandre Tutarashvili <aleksandre.tutarashvili.1@btu.edu.ge>
2022-06-26 20:32:22 +0400

    first commit

--- core/models.py
+++ core/models.py
@@ -5,7 +5,6 @@ from django.contrib.auth.models import User
 
 
 class Pdf(models.Model):
-    # user = models.ForeignKey(User , on_delete=models.CASCADE , default="")
     დასახელება = models.CharField(max_length=100)
     pdf = models.FileField(upload_to='Pdf')
 
